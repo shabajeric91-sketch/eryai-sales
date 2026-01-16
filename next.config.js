ef0b949c0979d0d4682fcce6eb35202ff75cb5b6
shabajeric91-sketch <shabajeric91@gmail.com>
2026-01-16 13:33:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // Server Actions är nu enabled by default i Next.js 14
+  // Behöver inte experimental flag längre
 }
 
 module.exports = nextConfig
